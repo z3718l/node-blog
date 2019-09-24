2e291a5ff4a7ba22238332ed7087f51156a804a5
Zhang Long <979463796@qq.com>
2019-09-24 14:18:45 +0800

    初始化路由

--- blog1/app.js
+++ blog1/app.js
@@ -1,13 +1,34 @@
+const handleBlogRouter = require('./src/router/blog')
+const handleUserRouter = require('./src/router/user')
+
 const serverHandle = (req, res) => {
   // 设置返回格式json
   res.setHeader('Content-type', 'application/json')
-  const resData = {
-    name: '111',
-    env: process.env.NODE_ENV
+  
+  // 处理blog路由
+  const blogData = handleBlogRouter(req, res)
+  if(blogData) {
+    res.end(
+      JSON.stringify(blogData)
+    )
+    return
   }
-  res.end(
-    JSON.stringify(resData)
-  )
+
+  // 处理user路由
+  const userData = handleUserRouter(req, res)
+  if(userData) {
+    res.end(
+      JSON.stringify(userData)
+    )
+    return
+  }
+
+  // 未命中路由返回404
+  res.writeHead(404, {"Content-type": "text/plain"})
+  res.write("404 Not Found\n")
+  res.end()
 }
 
 module.exports = serverHandle
+
+// env: process.env.NODE_ENV
